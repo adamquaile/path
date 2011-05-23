d46a2491cfbd37b6936bdefc2ab38fac3ce44953
Adam Quaile <adamquaile@gmail.com>
2011-05-23 22:05:25 +0100

    Fleshed out logic some more.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,9 @@
 
 
 void printUsage() {
+	
+	char* stLiteral = "Treat the directory . as literal - doesn't resolve before adding to PATH";
+
 	printf(\
 "\n\
 Usage: %s [command [options] filename] \n\
@@ -29,11 +32,58 @@ Usage: %s [command [options] filename] \n\
 		add: Adds the given filename to the path.\n\
 		remove: Removes the filename from the path, iff it is there\n\
 	\n\
+	Options:\n\
+	\n\
+		--literal: %s\
+	\n\
 "\
-, COMMAND_NAME);
+, COMMAND_NAME, stLiteral);
 }
 
-int main(int argc, const char* argv[]) {
+int main(int argc, char* argv[]) {	
+	if (argc < 2) {
+			printUsage();
+			return 0;
+	}
+	// Options to be set (and defaults)
+	// --literal 
+	bool opLiteral = false;
+	
+	// Get the filename (last argument)
+	char filename[40];
+	strcpy(filename, argv[argc-1]);
 
-	printUsage();
+	bool cAdd;
+	bool cRemove;
+	// Figure out which command was asked for
+	if (argv[1] == "add") {
+		cAdd = true;
+	} else if (argv[1] == "remove") {
+		cRemove = true;
+	}
+	
+	if (argc > 2) {
+		// We have some extra arguments
+		for (int i = 2; i < argc-1; i++) {
+			if (argv[i] == "--literal") {
+				opLiteral = true;
+			}
+		}
+	}
+	
+	// Here, we should know everything we need to
+	if (cAdd) {
+		if (opLiteral) {
+			addToPath(filename);
+		} else {
+			addToPath(resolvePath(filename));
+		}
+	}
+	
+	
+	char directory[100];
+	getCurrentDirectory(directory, sizeof(directory));
+	printf("%s", filename);
+	
 }
+
--- platforms/win7.cpp
+++ platforms/win7.cpp
@@ -1,7 +1,24 @@
 #include <stdlib.h>
-
-char* getCurrentPath() {
+	
+char* getCurrentPATH() {
 	char* pPath;
 	pPath = getenv("PATH");
 	return pPath;
 }
+
+// char* resolvePath(char* path) {
+// 	size_t = 100;
+// 	
+// }
+
+void getCurrentDirectory(char *directory, size_t size) {
+    getcwd(directory, size);
+}
+
+void addToPath(char *path) {
+	
+}
+void resolvePath(char *path) {
+	
+}
+
